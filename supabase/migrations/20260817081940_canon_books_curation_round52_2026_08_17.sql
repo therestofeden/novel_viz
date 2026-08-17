b1bb7b7435a9d26cb5df802ff65417d8763591a5
Stefano <stefano.restelli15@gmail.com>
2026-08-17 10:20:08 +0200

    canon curation round 52: Politics/Pensées/I Ching/Copernicus/Grimms' Fairy Tales close 5 gaps (746->755)

---
+++ supabase/migrations/20260817081940_canon_books_curation_round52_2026_08_17.sql
@@ -0,0 +1,10 @@
+insert into public.canon_books (title, author, source) values
+  ('Politics', 'Aristotle', 'curation_round52'),
+  ('Pensées', 'Blaise Pascal', 'curation_round52'),
+  ('Pensees', 'Blaise Pascal', 'curation_round52'),
+  ('I Ching', 'Anonymous', 'curation_round52'),
+  ('Book of Changes', 'Anonymous', 'curation_round52'),
+  ('On the Revolutions of the Heavenly Spheres', 'Nicolaus Copernicus', 'curation_round52'),
+  ('De revolutionibus orbium coelestium', 'Nicolaus Copernicus', 'curation_round52'),
+  ('Grimms'' Fairy Tales', 'Jacob and Wilhelm Grimm', 'curation_round52'),
+  ('Children''s and Household Tales', 'Jacob and Wilhelm Grimm', 'curation_round52');
